88bd22924bff66d65821981b75667917e4cf8221
ericsargsyan <eric.sargsiann@gmail.com>
2022-11-24 17:47:06 +0400

    prediction of our voices

--- infer.py
+++ infer.py
@@ -1,12 +1,11 @@
 import os
-import torch
 from dataset import GenderDataset
 from model import GenderClassificator
 import argparse
 from dataflow.utils import read_yaml
 import soundfile as sf
 import librosa
-import torchaudio
+import torch
 
 
 def arg_parser():
@@ -16,6 +15,7 @@ def arg_parser():
                         required=True)
     parser.add_argument('--audio_path',
                         type=str,
+                        nargs='+',
                         required=True)
     return parser.parse_args()
 
@@ -23,21 +23,27 @@ def arg_parser():
 if __name__ == "__main__":
     parser = arg_parser()
     config = read_yaml(parser.config_path)
-    audio_path = parser.audio_path
+    audio_paths = parser.audio_path
 
-    data_raw, samplerate = sf.read(audio_path)
-    data = librosa.resample(data_raw, target_sr=config['sr'], orig_sr=samplerate)
-    duration = data.shape[0] / config['sr']
-    data = torch.tensor(data[:config['sr'] * 2], dtype=torch.float32).view(1, -1)
+    audios = []
 
-    # data = torch.cat([data, data]) for multiple people
+    for audio_path in audio_paths:
+        data_raw, samplerate = sf.read(audio_path)
+        data = librosa.resample(data_raw, target_sr=config['sr'], orig_sr=samplerate)
+        duration = data.shape[0] / config['sr']
+        data = torch.tensor(data[:config['sr'] * 2], dtype=torch.float32).view(1, -1)
+        audios.append(data)
+
+    data = torch.cat(audios)
 
     check_path = '/Users/eric/Desktop/DL/Gender_Classification/logs/version_2/checkpoints/epoch==00-val_acc_epoch=0.944107.ckpt'
     model = GenderClassificator.load_from_checkpoint(check_path)
     model.eval()
-    y = model(data)
+    y_prob = model(data)
+    print(y_prob)
 
-    if y >= 0.5:
-        print(f'Label of {audio_path.split(os.sep)[-1]} - M')
-    else:
-        print(f'Label of {audio_path.split(os.sep)[-1]} - F')
+    for idx, y in enumerate(y_prob):
+        if y >= 0.5:
+            print(f'Label of {audio_paths[idx].split(os.sep)[-1]} - M')
+        else:
+            print(f'Label of {audio_paths[idx].split(os.sep)[-1]} - F')
