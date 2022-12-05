373721d647458f0a61887ca2782c995f4faa9197
ericsargsyan <eric.sargsiann@gmail.com>
2022-12-05 10:33:26 +0400

    model

--- model.py
+++ model.py
@@ -21,6 +21,12 @@ class GenderClassificator(pl.LightningModule):
         self.train_accuracy = torchmetrics.Accuracy()
         self.val_accuracy = torchmetrics.Accuracy()
         self.test_accuracy = torchmetrics.Accuracy()
+        self.train_f1score = torchmetrics.classification.BinaryF1Score()
+        self.val_f1score = torchmetrics.classification.BinaryF1Score()
+        self.test_f1score = torchmetrics.classification.BinaryF1Score()
+        self.train_auc = torchmetrics.classification.BinaryAUROC()
+        self.test_auc = torchmetrics.classification.BinaryAUROC()
+        self.val_auc = torchmetrics.classification.BinaryAUROC()
 
     def forward(self, x):
         x = self.mfcc(x)
@@ -33,9 +39,13 @@ class GenderClassificator(pl.LightningModule):
         loss = F.binary_cross_entropy(pred_probs, y)
         pred = pred_probs > 0.5
         batch_accuracy = self.train_accuracy(pred.int(), y.int())
+        train_f1score = self.train_f1score(pred.int(), y.int())
+        train_auroc = self.train_auc(pred.int(), y.int())
 
         self.log("train_acc", batch_accuracy, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=False, logger=True)
+        self.log('train_f1', train_f1score, on_step=True, on_epoch=True, prog_bar=False, logger=True)
+        self.log('train_auroc', train_auroc, on_step=True, on_epoch=True, prog_bar=False, logger=True)
 
         return loss
 
@@ -45,32 +55,60 @@ class GenderClassificator(pl.LightningModule):
         pred = pred_probs > 0.5
         self.val_accuracy(pred.int(), y.int())
         loss = F.binary_cross_entropy(self(x), y)
-        self.log("val_loss", loss)
+        val_f1score = self.val_f1score(pred.int(), y.int())
+        val_auroc = self.val_auc(pred.int(), y.int())
+
+        self.log("val_loss", loss, logger=True)
+        self.log('train_f1', val_f1score, on_step=True, on_epoch=True, prog_bar=False, logger=True)
+        self.log('train_auroc', val_auroc, on_step=True, on_epoch=True, prog_bar=False, logger=True)
 
     def test_step(self, test_batch, batch_idx):
         x, y = test_batch
         pred_probs = self(x)
         pred = pred_probs > 0.5
-        loss = F.binary_cross_entropy(self(x), y)
         acc = self.test_accuracy(pred.int(), y.int())
-        metrics = {"test_acc": acc, "test_loss": loss}
-        self.log_dict(metrics)
+        loss = F.binary_cross_entropy(self(x), y)
+        f1score = self.test_f1score(pred.int(), y.int())
+        roc_curve = self.test_auc(pred.int(), y.int())
+        metrics = {"Loss": loss,
+                   "Accuracy": acc}
+
+        # self.log_dict(metrics)
+
         return metrics
 
     def test_epoch_end(self, outputs):
         test_epoch_acc = self.test_accuracy.compute()
         self.test_accuracy.reset()
-        self.log("test_acc_epoch", test_epoch_acc, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        test_epoch_f1 = self.test_f1score.compute()
+        self.test_f1score.reset()
+        test_epoch_auroc = self.test_auc.compute()
+        self.test_auc.reset()
+        self.log("Accuracy_epoch", test_epoch_acc, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.log("F1_epoch", test_epoch_f1, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.log("AUC ROC_epoch", test_epoch_auroc, on_step=False, on_epoch=True, prog_bar=True, logger=True)
 
-    def train_epoch_end(self, outputs):  # ???
+    def train_epoch_end(self, outputs):
         train_epoch_acc = self.train_accuracy.compute()
         self.train_accuracy.reset()
+        train_epoch_f1 = self.train_f1score.compute()
+        self.train_f1score.reset()
+        train_epoch_auc = self.train_auc.compute()
+        self.train_auc.reset() ### train_stepum nayev logger-tensorboard
         self.log("train_acc_epoch", train_epoch_acc, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        self.log("train_f1-score_epoch", train_epoch_f1, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        self.log("train_auc_epoch", train_epoch_auc, on_step=True, on_epoch=True, prog_bar=True, logger=True)
 
     def validation_epoch_end(self, outputs):
         val_epoch_acc = self.val_accuracy.compute()
         self.val_accuracy.reset()
+        val_epoch_f1 = self.val_f1score.compute()
+        self.val_f1score.reset()
+        val_epoch_auc = self.val_auc.compute()
+        self.val_auc.reset()
         self.log("val_acc_epoch", val_epoch_acc, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.log("val_f1-score_epoch", val_epoch_f1, on_step=False, on_epoch=True, prog_bar=False, logger=True)
+        self.log("val_auc_epoch", val_epoch_auc, on_step=False, on_epoch=True, prog_bar=False, logger=True)
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=1e-2)
